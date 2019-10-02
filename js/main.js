85b5a2d684ca76ab7c036432f2bb792d3a1e5b30
ArkanBrd <dreskod7@icloud.com>
2019-10-02 18:54:17 +0700

    Личный проект: пока все дома

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// Файл main.js
+'use strict';
+
+var getRandomLike = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+var FLOODS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+
+var GUYS = ['Гуша', 'Курепа', 'МКС', 'Коняк', 'Аркан', 'Теплый'];
+
+var housePriceField = document.querySelector('#picture');
+
+var getRandomName = function () {
+  return GUYS[Math.floor(Math.random() * GUYS.length)];
+};
+var getRandomComments = function () {
+  return FLOODS[Math.floor(Math.random() * FLOODS.length)];
+};
+
+var descriptionPhotos = function () {
+  var photos = [];
+  for (var i = 0; i < 25; i++) {
+    var photo = {};
+    photo.url = photos/i.jpg;
+    photo.description = ' ';
+    photo.likes = getRandomLike(15, 200);
+    photo.comments = [];
+    for (var j = 0; j < 6 ; j++) {
+      var comment = {};
+	  comments.avatar = img/j.svg;
+	  comments.message = getRandomComments();
+	  comments.name = getRandomName();
+	  comments.push(comment);
+	};
+    photos.push(photo);
+  }
+  return photos;
+};
+
+var picture = descriptionPhotos();
