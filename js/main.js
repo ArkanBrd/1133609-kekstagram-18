a4e4446f2e3aeb0dbe5441e3f4fff62e0b0a502f
ArkanBrd <dreskod7@icloud.com>
2019-10-06 22:13:38 +0700

    разбираюсь с DOM

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Файл main.js
 'use strict';
 
-var getRandomLike = function(min, max) {
+var getRandomLike = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
@@ -10,37 +10,45 @@ var FLOODS = ['Всё отлично!', 'В целом всё неплохо. Н
 var GUYS = ['Гуша', 'Курепа', 'МКС', 'Коняк', 'Аркан', 'Теплый'];
 
 var housePriceField = document.querySelector('#picture');
+housePriceField.classList.remove('hidden');
 
 var getRandomName = function () {
   return GUYS[Math.floor(Math.random() * GUYS.length)];
 };
 var getRandomComments = function () {
   return FLOODS[Math.floor(Math.random() * FLOODS.length)];
-  return FLOODS[Math.floor(Math.random() * FLOODS.length)];
 };
- var generateComments = function () {
+var generateComments = function () {
   var coments = [];
-  for (var j = 0; j < 6 ; j++) {
-    coment = {
-      avatar: "img/avatar-" + j + ".svg";
-      message: getRandomComments();
-      name: getRandomName();
-	};
-	return coments;
-  };
- };
- 
+  for (var j = 0; j < 6; j++) {
+    var coment = {
+      avatar: 'img/avatar-' + j + '.svg',
+      message: getRandomComments() + '. ' + getRandomComments(),
+      name: getRandomName()
+    };
+    coments.push(coment);
+  }
+  return coments;
+};
+
 var generatePhotoDescriptions = function () {
   var photos = [];
   for (var i = 0; i < 25; i++) {
     var photo = {
-      url: "photos/" + i + ".jpg";
-      description: '...';
-      likes: getRandomLike(15, 200);
-      comments: generateComments();
-	};
-  };
+      url: 'photos/' + i + '.jpg',
+      description: '...',
+      likes: getRandomLike(15, 200),
+      comments: generateComments()
+    };
+    photos.push(photo);
+  }
   return photos;
 };
 
-var picture = generatePhotoDescriptions();
+var pictures = generatePhotoDescriptions();
+var renderPicture = function (picture) {
+  pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+  pictureElement.querySelector('.picture__comments').textContent = picture.comments;
+  return renderPicture;
+};
+
