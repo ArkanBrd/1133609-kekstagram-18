101583e4e2ace8b85aed4107e7f6cbe83503f85f
ArkanBrd <dreskod7@icloud.com>
2019-11-15 01:01:30 +0700

    17. Личный проект: перламутровые пуговицы

--- js/gallery.js
+++ js/gallery.js
@@ -14,13 +14,92 @@
     mainPhoto.appendChild(errorElement);
   };
 
-  var onSuccess = function (data) {
-    pictures = data;
+  var filterName = 'popular';
+  var filterPopular = function () {
+    return pictures;
+  };
+  var filterRandom = function () {
+    var pictureRandom = [];
+    var picturesCopy = pictures.slice();
+    for (var i = 0; i < 10 && picturesCopy.length > 0; i++) {
+      var randomNumber = Math.floor(Math.random() * picturesCopy.length);
+      pictureRandom.push(picturesCopy[randomNumber]);
+      picturesCopy.splice(randomNumber, 1);
+    }
+    return pictureRandom;
+  };
+  var filterDiscussed = function () {
+    var picturesCopy = pictures.slice();
+    picturesCopy.sort(function (a, b) {
+      return b.comments.length - a.comments.length;
+    });
+    return picturesCopy;
+  };
+
+  var buttonFilter = {
+    popular: {
+      element: document.querySelector('#filter-popular'),
+      method: filterPopular,
+    },
+    random: {
+      element: document.querySelector('#filter-random'),
+      method: filterRandom,
+    },
+    discussed: {
+      element: document.querySelector('#filter-discussed'),
+      method: filterDiscussed,
+    },
+  };
+
+  var forButton = function (argument) {
+    var classPicture = uploadPhotoElement.querySelectorAll('.picture');
+    for (var j = 0; j < classPicture.length; j++) {
+      uploadPhotoElement.removeChild(classPicture[j]);
+    }
+    buttonFilter[filterName].element.classList.remove('img-filters__button--active');
+    var newPicture = buttonFilter[argument].method();
+    filterName = argument;
+    buttonFilter[argument].element.classList.add('img-filters__button--active');
     var fragment = document.createDocumentFragment();
-    for (var i = 0; i < pictures.length; i++) {
-      fragment.appendChild(window.picture.renderPicture(pictures[i], i));
+    for (var i = 0; i < newPicture.length; i++) {
+      fragment.appendChild(window.picture.renderPicture(newPicture[i], i));
     }
     uploadPhotoElement.appendChild(fragment);
+  };
+
+  var lastTimeout;
+
+  var timeButton = function (argument) {
+    if (lastTimeout) {
+      window.clearTimeout(lastTimeout);
+    }
+    lastTimeout = setTimeout(function () {
+      forButton(argument);
+      lastTimeout = null;
+    }, 500);
+  };
+
+  var filterButtonPopular = function () {
+    timeButton('popular');
+  };
+
+  var filterButtonRandom = function () {
+    timeButton('random');
+  };
+
+  var filterButtonDiscussed = function () {
+    timeButton('discussed');
+  };
+
+
+  var onSuccess = function (data) {
+    pictures = data;
+    forButton('popular');
+    var imgFilters = document.querySelector('.img-filters');
+    imgFilters.classList.remove('img-filters--inactive');
+    buttonFilter.popular.element.addEventListener('click', filterButtonPopular);
+    buttonFilter.random.element.addEventListener('click', filterButtonRandom);
+    buttonFilter.discussed.element.addEventListener('click', filterButtonDiscussed);
   };
 
   var pictures = [];
@@ -37,5 +116,6 @@
   });
 
   firstuploadFormOpen.addEventListener('change', window.form.uploadFormOpen);
+
 
 })();
